85205762a40098c2985431bba960a0f2e62bf126
李鹏飞 <murphy573@Lipf.local>
2020-09-11 15:55:51 +0800

    feat：添加页面切换方向监听，增加切换动画

--- examples/App.vue
+++ examples/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <VuePageNavigation :include="include">
-      <router-view />
-    </VuePageNavigation>
+    <transition :name="transitionName">
+      <VuePageNavigation :include="include">
+        <router-view />
+      </VuePageNavigation>
+    </transition>
   </div>
 </template>
 
@@ -10,8 +12,16 @@
 export default {
   name: 'App',
 
+  watch: {
+    $route () {
+      let d = this.$route.params.PNKDirection;
+      this.transitionName = d === 'forward' ? 'van-slide-right' : 'van-slide-left';
+    }
+  },
+
   data () {
     return {
+      transitionName: 'van-fade',
       include: ['Page1']
     };
   }
@@ -25,5 +35,79 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  @keyframes van-slide-left-enter {
+    from {
+      transform: translate3d(-100%, 0, 0);
+    }
+  }
+
+  @keyframes van-slide-left-leave {
+    to {
+      transform: translate3d(-100%, 0, 0);
+    }
+  }
+
+  @keyframes van-slide-right-enter {
+    from {
+      transform: translate3d(100%, 0, 0);
+    }
+  }
+
+  @keyframes van-slide-right-leave {
+    to {
+      transform: translate3d(100%, 0, 0);
+    }
+  }
+
+  @keyframes van-fade-in {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes van-fade-out {
+    from {
+      opacity: 1;
+    }
+
+    to {
+      opacity: 0;
+    }
+  }
+
+  .van-fade {
+    &-enter-active {
+      animation: 0.3s van-fade-in both ease-out;
+    }
+
+    &-leave-active {
+      animation: 0.3s van-fade-out both ease-in;
+    }
+  }
+
+  .van-slide-left {
+    &-enter-active {
+      animation: van-slide-left-enter 0.3s both ease-out;
+    }
+
+    &-leave-active {
+      animation: van-slide-left-leave 0.3s both ease-in;
+    }
+  }
+
+  .van-slide-right {
+    &-enter-active {
+      animation: van-slide-right-enter 0.3s both ease-out;
+    }
+
+    &-leave-active {
+      animation: van-slide-right-leave 0.3s both ease-in;
+    }
+  }
 }
 </style>
